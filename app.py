a3c01c0ca6e34819ed0d7b6f3fd9b2ec60053832
Santiago21112001 <minesanti2001@gmail.com>
2024-06-01 15:34:58 +0200

    Pasado el editor de circuitos a una clase aparte.

--- app.py
+++ app.py
@@ -1,33 +1,33 @@
 import tkinter as tk
-from tkinter import messagebox
+from tkinter import ttk
 
-from circuit_pieces.draggable_arc import DraggableArc
-from circuit_pieces.draggable_rectangle import DraggableRectangle
-from circuit_pieces.draggable_piece import DraggablePiece
-from circuit_pieces.draggable_polygon import DraggablePolygon
-from file_manager import FileManager
+from circuits_editor import CircuitsEditor
 
 
 class App:
     def __init__(self):
         """
         Constructor for App.
-        Opens the main window and reads the JSON file that is in the same directory.
         """
         self.selected_piece = None
         self.root = tk.Tk()
         self.root.title("Editor de circuitos y robots")
+        width = 1024
+        height = 768
+        self.root.geometry(str(width)+"x"+str(height))
         self.root.resizable(False, False)
 
-        self.canvas = tk.Canvas(self.root, width=1280, height=720)
-        self.canvas.pack()
+        # Contenedor principal para los frames
+        self.container = ttk.Frame(self.root)
+        self.container.pack(fill="both", expand=True)
+
+        # Instanciar todas las ventanas
+        self.editor = CircuitsEditor(self.container, width, height)
+
         self.menu_bar = self.create_menu()
-        self.create_buttons()
-        self.draggable_pieces = []
 
-        self.file_manager = FileManager()
-        self.file_content = self.file_manager.open_circuits_file()
-
+        self.editor.frame.grid(row=0, column=0, sticky="nsew")
+        self.editor.frame.tkraise()
         self.root.mainloop()
 
     def create_menu(self):
@@ -44,104 +44,8 @@ class App:
 
         return menu_bar
 
-    def create_buttons(self):
-        add_rectangle_button = tk.Button(self.root, text="Añadir recta horizontal", command=self.add_rectangle,
-                                         bg="green", fg="white", padx=10, pady=5)
-        add_rectangle_button.pack(side=tk.RIGHT, padx=10, pady=10)
-
-        add_rectangle_y_button = tk.Button(self.root, text="Añadir recta vertical", command=self.add_rectangle_y,
-                                           bg="green", fg="white", padx=10, pady=5)
-        add_rectangle_y_button.pack(side=tk.RIGHT, padx=10, pady=10)
-
-        add_arc_button = tk.Button(self.root, text="Añadir curva", command=self.add_arc, bg="green",
-                                   fg="white", padx=10, pady=5)
-        add_arc_button.pack(side=tk.RIGHT, padx=10, pady=10)
-
-        add_polygon_button = tk.Button(self.root, text="Añadir cruce", command=self.add_polygon, bg="green",
-                                       fg="white", padx=10, pady=5)
-        add_polygon_button.pack(side=tk.RIGHT, padx=10, pady=10)
-
-        clear_canvas_button = tk.Button(self.root, text="Limpiar lienzo", command=self.clear_canvas, bg="green",
-                                        fg="white", padx=10, pady=5)
-        clear_canvas_button.pack(side=tk.RIGHT, padx=10, pady=10)
-
-        rotate_button = tk.Button(self.root, text="Rotar", command=self.rotate, bg="green",
-                                  fg="white", padx=10, pady=5)
-        rotate_button.pack(side=tk.RIGHT, padx=10, pady=10)
-
-    def add_rectangle(self):
-        dist = 70
-        x1 = 100
-        y1 = 100
-        new_piece: DraggablePiece = DraggableRectangle(self, x1, y1, "x", dist)
-        self.draggable_pieces.append(new_piece)
-
-    def add_rectangle_y(self):
-        dist = 70
-        x1 = 100
-        y1 = 100
-        new_piece: DraggablePiece = DraggableRectangle(self, x1, y1, "y", dist)
-        self.draggable_pieces.append(new_piece)
-
-    def add_arc(self):
-        dist = 70
-        x1 = 100
-        y1 = 100
-        new_piece: DraggablePiece = DraggableArc(self, x1, y1, dist)
-        self.draggable_pieces.append(new_piece)
-
-    def add_polygon(self):
-        x = 100
-        y = 100
-        new_piece: DraggablePiece = DraggablePolygon(self, x, y)
-        self.draggable_pieces.append(new_piece)
-
     def open_file(self):
-        file_content = self.file_manager.open_file()
-        if file_content is None:
-            return
-        self.file_content = file_content
-        self.append_file_pieces(file_content)
-
-    def append_file_pieces(self, file_content):
-        """Draws the circuit parts of the JSON file."""
-        parts = file_content["circuits"][0]["parts"]
-        self.draggable_pieces = []
-        for part in parts:
-            part_type = part['type']
-            x1 = part['x1']
-            y1 = part['y1']
-            piece = None
-            if part_type == 'turn':
-                piece = DraggableArc(self, x1, y1, part['dist'], part['start'], part['extent'])
-            elif part_type == 'polygon':
-                piece = DraggablePolygon(self, x1, y1)
-            elif part_type == 'straight':
-                piece = DraggableRectangle(self, x1, y1, part['orient'], part['dist'])
-            if piece is not None:
-                self.draggable_pieces.append(piece)
+        self.editor.open_file()
 
     def save_file(self):
-        parts_json = [piece.get_piece_info() for piece in self.draggable_pieces]
-        # Partes del primer circuito
-        self.file_content["circuits"][0]["parts"] = parts_json
-        self.file_manager.save_circuits_file(self.file_content)
-        messagebox.showinfo("Archivo guardado", "Se ha guardado el archivo 'circuits.json' en el directorio raíz de "
-                                                "la aplicación")
-
-    def clear_canvas(self):
-        self.draggable_pieces.clear()
-        self.selected_piece = None
-        self.canvas.delete("all")
-
-    def set_select_piece(self, piece: DraggablePiece):
-        if self.selected_piece is not None:
-            self.selected_piece.set_outline("black")
-        self.selected_piece = piece
-        piece.set_outline("red")
-
-    def rotate(self):
-        if self.selected_piece is None:
-            messagebox.showwarning("Advertencia", "Selecciona una pieza")
-        else:
-            self.selected_piece.rotate()
+        self.editor.save_file()
--- file_manager.py
+++ file_manager.py
@@ -1,21 +1,10 @@
-from tkinter import filedialog
 import json
-import os
+from tkinter import filedialog
 
 
 class FileManager:
     def __init__(self):
         pass
-
-    def open_circuits_file(self):
-        file_path = "circuits.json"
-        if file_path:
-            with open(file_path, 'r') as file:
-                content = json.load(file)
-                print("Archivo abierto")
-            return content
-        else:
-            return None
 
     def open_file(self):
         file_path = filedialog.askopenfilename(filetypes=[("Archivos JSON", "*.json")])
@@ -27,8 +16,7 @@ class FileManager:
         else:
             return None
 
-    def save_circuits_file(self, content):
-        file_path = "circuits.json"
+    def save_file(self, content, file_path):
         with open(file_path, 'w') as file:
             json.dump(content, file, indent=4)
         print(f"Se ha guardado el archivo JSON como '{file_path}'.")
